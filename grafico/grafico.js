6f23fbfce243ebe83c12c330162cf3f4793c23df
gueba14 <santiagoguebara@gmail.com>
2025-06-28 16:49:07 -0300

    s

--- grafico/grafico.js
+++ grafico/grafico.js
@@ -302,3 +302,13 @@ const menuNavegacion = document.getElementById('menu-navegacion');
 btnHamburguesa.addEventListener('click', () => {
   menuNavegacion.classList.toggle('activo');
 });
+window.onscroll = function() {
+  const btn = document.getElementById("btn-Subir");
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) 
+  {
+    btn.style.display = "block";
+  } else 
+  {
+    btn.style.display = "none";
+  }
+};
